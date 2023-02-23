058605e5ded54c686b6d5b1dcec401ec5ce5ba90
KamilCzachor <114612495+KamilCzachor@users.noreply.github.com>
2023-02-23 16:15:22 +0100

    Add files via upload

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const itemList = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemList.length}`);
+itemList.forEach((item) => {
+  console.log(`Categories: ${item.firstElementChild.textContent} `);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
